d50f6f838b128f5d513750f2e8096d37bb1f9ae3
Raunak Pandey <65064180+RAUNAK-PANDEY@users.noreply.github.com>
2021-07-27 23:59:38 +0530

    inline css

--- src/index.js
+++ src/index.js
@@ -34,10 +34,35 @@ document.getElementById("root").appendChild(h1);
 const name = "Raunak";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
+const currentTime = currentDate.getHours();
+
+let greeting;
+
+/* Use and properties of inline CSS */
+const inlineStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  inlineStyle.color = "green";
+} else if (currentTime > 12 && currentTime < 18) {
+  greeting = "Good Afternoon";
+  inlineStyle.color = "blue";
+} else {
+  greeting = "Good Night";
+  inlineStyle.color = "red";
+}
+
 //Can use only expressions ex- {Math.floor(Math.random() * 10)} not statements ex- if else
+
 ReactDOM.render(
   <div>
-    <h1> Hello {name} </h1>
+    <h1 className="inline" style={inlineStyle}>
+      {" "}
+      {greeting}{" "}
+    </h1>
+    <h2> Hello {name} </h2>
     <p> Lucky number is {Math.floor(Math.random() * 10)} </p>
     <p> Copyright {year} </p>
   </div>,
