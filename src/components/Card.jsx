4ea1929f20dc34f2e67527b5cf8531a993030b72
Raunak Pandey <65064180+RAUNAK-PANDEY@users.noreply.github.com>
2021-07-30 00:48:20 +0530

    Smaller Components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 //Use of React props
 function Card(props) {
@@ -6,11 +8,11 @@ function Card(props) {
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Detail detailInfo={props.email} />
+        <Detail detailInfo={props.tel} />
       </div>
     </div>
   );
